4be138d0ab54bd12fe71f2d13ea98a268c955c26
umar-abdullah22 <umar.coderzhunt@gmail.com>
2023-11-13 14:59:25 +0500

    remove consoles for error

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -1,6 +1,4 @@
 exports.errorHandler = (err, req, res, next) => {
-  console.error('Error:sdsdsdsdsdsdsd', err);
-  console.log('hgeyrguwkyg');
   // Handle other errors with a generic message
   res.status(500).json({ message: err });
 };
